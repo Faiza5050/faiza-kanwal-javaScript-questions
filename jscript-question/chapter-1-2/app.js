43955848232c8b859f9e5a269acc49a1d25fc356
FAIZA KANWAL <155901304+Faiza5050@users.noreply.github.com>
2024-09-30 18:15:34 +0500

    Add files via upload

---
+++ jscript-question/chapter-1-2/app.js
@@ -0,0 +1,66 @@
+// Chapter # 1 Alert Chapter
+
+// Question # 1:
+
+// In JavaScript, the alert function displays a dialog box with a specified message and an OK button.
+
+
+// // Question # 2:
+
+// alert("Welcome to my website!");
+
+
+// Question # 3:
+
+// JavaScript is a case-sensitive, if we capitalize the alert keyword as Alert, it will not work.
+
+
+// // Question # 4:
+
+// alert("Hello, world!");
+
+
+// // Question # 5:
+
+// alert("Hello World");
+// 1- alert: alert is a JavaScript function it is used to display an alert box to the user.
+// 2- (): In Parentheses, we are passing the single argument to the alert function.
+// 3- string"": It displays in alert box when we write anything inside "string", we can use string in single quote '' or double quotes "".
+// 4- Text: We will see text in the alert box, the text is written inside the quotes.
+
+
+// Chapter # 2 Variables for Strings
+
+// // Question # 1:
+
+// var message = "Hello, world!";
+// alert(message);
+
+
+// // Question # 2:
+
+// var age;
+// age = 25;
+// alert(age);
+
+
+// // Question # 3:
+
+// var greeting =  "Good morning!";
+// alert(greeting);
+
+// greeting = "Good evening!";
+// alert(greeting);
+
+
+// // Question # 4:
+
+// var favoriteColor = "blue";
+// alert("My favorite color is " + favoriteColor);
+
+
+// // Question # 5:
+
+// var age;
+// age = 25;
+// alert(age);
